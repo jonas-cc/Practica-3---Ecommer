aaa016167a5687f0fcfac8cf170d2898a4977373
jonas <jonas_cc@tesch.edu.mx>
2022-11-29 17:16:40 -0600

    primer commit

---
+++ practica2.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3977;
+
+
+mongoose.connect('mongodb://localhost:27017/proyecto', (err, res) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log("conexión exitosa");
+        app.listen(port, function() {
+            console.log("Servidor de api rest de musica eschuando en http://localhost:" + port);
+        });
+
+    }
+})
